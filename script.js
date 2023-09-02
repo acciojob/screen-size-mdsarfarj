c273d916b99fed7d53ebf8912673903b779d187f
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-09-02 17:23:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+ function updateWindowSize() {
+            const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+            const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+            // Update the content with the current window size
+            document.getElementById('width').textContent = width;
+            document.getElementById('height').textContent = height;
+        }
+
+        // Initial call to set the window size
+        updateWindowSize();
+
+        // Attach an event listener to update the size whenever the window is resized
+        window.addEventListener('resize', updateWindowSize);
+        // Attach an event listener to update the size whenever the window is resized
+        window.addEventListener('resize', updateWindowSize);
